03ddfd9dc5120b10bd08b38fae4d6dd9f49bf957
Trong Van <trongvan214@yahoo.com>
2017-09-04 21:02:42 -0500

    change ajax request method

--- main.js
+++ main.js
@@ -28,77 +28,92 @@ $(document).ready(function(){
             wantedFormatIn = "http://api.openweathermap.org/data/2.5/weather?zip=";
         }
         var targetUrl = wantedFormatIn+userValue+key+units;           //full url build base on user inputs
-        $.getJSON(targetUrl, function(data){
-            var lat = data.coord.lat;
-            var lon = data.coord.lon;
-            var temp = data.main.temp;
-            var humidity = data.main.humidity;
-            var pressure = data.main.pressure;
-            var windSpeed = data.wind.speed;
-            var weatherDescription = data.weather[0].description;
-            var iconCode = data.weather[0].icon;
-            var cityName = data.name;
-            var iconUrl = "http://openweathermap.org/img/w/"+iconCode+".png";
-            $('#location-info').html(cityName);
-            $('#weather-description').html(weatherDescription);
-            $('#temperature h1').html(Math.round(temp));
-            $('#humidity span').html('Humidity: ' + humidity + '%');
-            $('#wind-speed span').html('Wind: '+ windSpeed + " MPH");
-            $('#icon').css('background-image', 'url('+iconUrl+')');
-            $('main').removeClass('hide');
-            $('span.b1').on('click', function(){                         //switching degree and the math to convert 
-                if($('.b2').hasClass('active')){
-                    $('.b2').removeClass('active');
-                    $('.b1').addClass('active');
-                    $('#temperature h1').html(Math.round(temp));
-                }
-            });
-            $('span.b2').on('click', function(){
-                if($('.b1').hasClass('active')){
-                    $('.b1').removeClass('active');
-                    $('.b2').addClass('active');
-                }
-                $('#temperature h1').html(Math.round((temp-32)*(5/9)));
-            });
-            timeInfo(lat,lon);                                         //function that return current time of that location
-        });
+        $.ajax({
+            method: 'get',
+            url: targetUrl,
+            success: function(data){
+                getWeather(data);
+            }
+        })                                        //function that return current time of that location
     }); 
-    function timeInfo(lat, lon){
-        var currentTimeInSec = Math.round(new Date().getTime()/1000);
-        const googlePrototypeUrl = "https://maps.googleapis.com/maps/api/timezone/json?";
-        const location = "location="+lat+","+lon;
-        const timeStamp = "&timestamp="+currentTimeInSec;
-        const key = "&key=AIzaSyDPLledCWfdfdmt0L4k_7chldSYd5tCATs";
-        var googleTimeZoneUrl = googlePrototypeUrl+location+timeStamp+key;
-        $.getJSON(googleTimeZoneUrl, function(data){
-            var hoursDifferent = (data.rawOffset+data.dstOffset)/3600;
-            var currentHours = function(){
-                var UTCHours = new Date().getUTCHours();
-                if(UTCHours >= 0  && UTCHours < 12 && hoursDifferent < 0) {
-                    return Math.round((UTCHours+24)+hoursDifferent);
-                }
-                else 
-                {
-                    return Math.round(UTCHours+hoursDifferent);
-                }
-            }
-            var currentMinutes = new Date().getMinutes();
-            if(currentHours() > 12) {                                     //24hours format time (solution)
-                if (currentMinutes < 10){                                 //single digit cases 
-                    $('#time-info').html(currentHours()%12+":0"+currentMinutes+" PM");
-                }
-                else {
-                    $('#time-info').html(currentHours()%12+":"+currentMinutes+" PM");
-                }
-            }
-            else {
-                if (currentMinutes < 10){
-                    $('#time-info').html(currentHours()+":0"+currentMinutes+" AM");
-                }
-                else {
-                    $('#time-info').html(currentHours()+":"+currentMinutes+" AM");  
-                }
-            }
-        });
+});
+function getWeather(data){
+    var lat = data.coord.lat;
+    var lon = data.coord.lon;
+    var temp = data.main.temp;
+    var humidity = data.main.humidity;
+    var pressure = data.main.pressure;
+    var windSpeed = data.wind.speed;
+    var weatherDescription = data.weather[0].description;
+    var iconCode = data.weather[0].icon;
+    var cityName = data.name;
+    var iconUrl = "http://openweathermap.org/img/w/"+iconCode+".png";
+    $('#location-info').html(cityName);
+    $('#weather-description').html(weatherDescription);
+    $('#temperature h1').html(Math.round(temp));
+    $('#humidity span').html('Humidity: ' + humidity + '%');
+    $('#wind-speed span').html('Wind: '+ windSpeed + " MPH");
+    $('#icon').css('background-image', 'url('+iconUrl+')');
+    $('main').removeClass('hide');
+    $('span.b1').on('click', function(){                         //switching degree and the math to convert 
+        if($('.b2').hasClass('active')){
+            $('.b2').removeClass('active');
+            $('.b1').addClass('active');
+            $('#temperature h1').html(Math.round(temp));
+        }
+    });
+    $('span.b2').on('click', function(){
+        if($('.b1').hasClass('active')){
+            $('.b1').removeClass('active');
+            $('.b2').addClass('active');
+        }
+        $('#temperature h1').html(Math.round((temp-32)*(5/9)));
+    });
+    timeInfo(lat,lon); 
+}
+function timeInfo(lat, lon){
+    var currentTimeInSec = Math.round(new Date().getTime()/1000);
+    const googlePrototypeUrl = "https://maps.googleapis.com/maps/api/timezone/json?";
+    const location = "location="+lat+","+lon;
+    const timeStamp = "&timestamp="+currentTimeInSec;
+    const key = "&key=AIzaSyDPLledCWfdfdmt0L4k_7chldSYd5tCATs";
+    var googleTimeZoneUrl = googlePrototypeUrl+location+timeStamp+key;
+    $.ajax({
+        method: 'get',
+        url: googleTimeZoneUrl,
+        success: function(data){
+            getCurrentTime(data);
+        }
+    });
+ }
+
+function getCurrentTime(data){
+    var hoursDifferent = (data.rawOffset+data.dstOffset)/3600;
+    var currentHours = function(){
+        var UTCHours = new Date().getUTCHours();
+        if(UTCHours >= 0  && UTCHours < 12 && hoursDifferent < 0) {
+            return Math.round((UTCHours+24)+hoursDifferent);
+        }
+        else 
+        {
+            return Math.round(UTCHours+hoursDifferent);
+        }
     }
-}); 
+    var currentMinutes = new Date().getMinutes();
+    if(currentHours() > 12) {                                     //24hours format time (solution)
+        if (currentMinutes < 10){                                 //single digit cases 
+            $('#time-info').html(currentHours()%12+":0"+currentMinutes+" PM");
+        }
+        else {
+            $('#time-info').html(currentHours()%12+":"+currentMinutes+" PM");
+        }
+    }
+    else {
+        if (currentMinutes < 10){
+            $('#time-info').html(currentHours()+":0"+currentMinutes+" AM");
+        }
+        else {
+            $('#time-info').html(currentHours()+":"+currentMinutes+" AM");  
+        }
+    }
+}
