046672d7a003e8ba9a46985a8e94f1999ce54ec0
Trong Van <trongvan214@yahoo.com>
2017-09-04 19:49:53 -0500

    responsive added

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
             searchInputBox.attr('placeholder', 'Exp: 67211');
         }
     });
-    $('#submit').on('click', function(){       
+    $('#submit').on('click', function(){  
         var wantedFormatIn, units = "&units=imperial";              //parameters of url query
         const key = "&APPID=1498bed46b956aa47fc7d61fef70fa32";
         var userValue = $('#search').val();
@@ -39,31 +39,30 @@ $(document).ready(function(){
             var iconCode = data.weather[0].icon;
             var cityName = data.name;
             var iconUrl = "http://openweathermap.org/img/w/"+iconCode+".png";
-            $('#temp-value').append('<div class="switch-degree"><h6 class="button active b1">&deg;F</h6> | <h6 class="button b2">&deg;C</h6></div>');
             $('#location-info').html(cityName);
             $('#weather-description').html(weatherDescription);
-            $('#temp-value span').html(Math.round(temp));
+            $('#temperature h1').html(Math.round(temp));
             $('#humidity span').html('Humidity: ' + humidity + '%');
-            $('#wind-speed span').html('Wind: '+ windSpeed + "MPH");
+            $('#wind-speed span').html('Wind: '+ windSpeed + " MPH");
             $('#icon').css('background-image', 'url('+iconUrl+')');
-            $('#humidity img, #wind-speed img').removeClass('hide');
-            $('h6.b1').on('click', function(){                         //switching degree and the math to convert 
+            $('main').removeClass('hide');
+            $('span.b1').on('click', function(){                         //switching degree and the math to convert 
                 if($('.b2').hasClass('active')){
                     $('.b2').removeClass('active');
                     $('.b1').addClass('active');
-                    $('#temp-value span').html(Math.round(temp));
+                    $('#temperature h1').html(Math.round(temp));
                 }
             });
-            $('h6.b2').on('click', function(){
+            $('span.b2').on('click', function(){
                 if($('.b1').hasClass('active')){
                     $('.b1').removeClass('active');
                     $('.b2').addClass('active');
                 }
-                $('#temp-value span').html(Math.round((temp-32)*(5/9)));
+                $('#temperature h1').html(Math.round((temp-32)*(5/9)));
             });
             timeInfo(lat,lon);                                         //function that return current time of that location
         });
-    });
+    }); 
     function timeInfo(lat, lon){
         var currentTimeInSec = Math.round(new Date().getTime()/1000);
         const googlePrototypeUrl = "https://maps.googleapis.com/maps/api/timezone/json?";
@@ -102,4 +101,4 @@ $(document).ready(function(){
             }
         });
     }
-});
+}); 
